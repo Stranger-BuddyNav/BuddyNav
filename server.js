bc332c52a649cdda851d49dc73d71782a7f1defb
AnwarAbbass <9982017726.UPS@htu.edu.jo>
2021-04-17 14:13:24 +0300

    add basic line for server

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const DATABASE_URL = process.env.DATABASE_URL;
+const NODE_ENV = process.env.NODE_ENV;
+const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
+const client = new pg.Client(options);
+
+const methodOverride = require('method-override');
+
+// setupes
+const PORT = process.env.PORT || 3030;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(methodOverride('_method'));
+
+// unfound route
+app.get('*', (req, res) => res.status(404).send('This route does not exist'));
+
+// routes
+
+
+
+//constructors
+
+
+
+//listen
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+  });
+
