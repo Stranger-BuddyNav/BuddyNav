4269addba8a83a652e24a3d98e7639cbdcbba3a9
mostafakjdeitawi <mostafagedo0785@gmail.com>
2021-04-18 13:24:51 +0300

    covid route

--- server.js
+++ server.js
@@ -20,20 +20,44 @@ app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 app.use(methodOverride('_method'));
 
-// unfound route
-app.get('*', (req, res) => res.status(404).send('This route does not exist'));
-
 // routes
 
+app.get('/',(req,res) => {
+  res.send('your server ready to use !!');
+});
+
+app.get('/covid',(req,res) => {
+  let countryName = req.query.country;
+  let capitlizedCountry = countryName.charAt(0).toUpperCase() + countryName.slice(1);
+  let url = `https://covid-api.mmediagroup.fr/v1/cases?country=${capitlizedCountry}`;
+  superagent.get(url)
+    .then(result => {
+      // console
+      let gettedData = result.body.All;
+      let newCountry = new Covid (gettedData);
+      res.send(newCountry);
+    });
+});
 
 
 //constructors
+function Covid (covidData){
+  this.country = covidData.country;
+  this.population = covidData.population;
+  this.confirmed = covidData.confirmed;
+  this.recovered = covidData.recovered;
+  this.deaths = covidData.deaths;
+  this.updated = covidData.updated;
+}
 
+
+// unfound route
+app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
 
 //listen
-client.connect()
-  .then(() => {
-    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
-  });
+// client.connect()
+//   .then(() => {
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+// });
 
