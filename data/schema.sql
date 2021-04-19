1f39fb55723cb804cb1a8ca02dc22a95819dfa9f
AnwarAbbass <9982017726.UPS@htu.edu.jo>
2021-04-20 01:40:11 +0300

    create database tabels

--- data/schema.sql
+++ data/schema.sql
@@ -1,9 +1,57 @@
+DROP TABLE IF EXISTS city;
+CREATE TABLE city (
+  id SERIAL PRIMARY KEY,
+  city VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS locationdata;
+CREATE TABLE locationdata (
+  id SERIAL PRIMARY KEY,
+  city_id int,
+  map_url VARCHAR(255),
+  city_url VARCHAR(255),
+  FOREIGN KEY (city_id) REFERENCES city(id)
+);
+
+DROP TABLE IF EXISTS hoteldata;
+CREATE TABLE hoteldata (
+  id SERIAL PRIMARY KEY,
+  city_id int,
+  hotel_image VARCHAR(255),
+  hotel_name VARCHAR(255),
+  hotel_price VARCHAR(255),
+  hotel_rate VARCHAR(255),
+  FOREIGN KEY (city_id) REFERENCES city(id)
+);
+
+DROP TABLE IF EXISTS transportdata;
+CREATE TABLE transportdata (
+  id SERIAL PRIMARY KEY,
+  city_id int,
+  station_name VARCHAR(255),
+  station_type VARCHAR(255),
+  transport_price VARCHAR(255),
+  FOREIGN KEY (city_id) REFERENCES city(id)
+);
+
+
+
+-- buddynav=# select * from hoteldata join city on hoteldata.city_id=city.id join transportdata on transportdata.city_id=city.id where city.id=5;
+
+
+
 DROP TABLE IF EXISTS booking;
 CREATE TABLE booking (
   id SERIAL PRIMARY KEY,
   city VARCHAR(255),
-  image_url VARCHAR(255),
-  date VARCHAR(255),
-  hotel VARCHAR(255),
-  transport VARCHAR(255)
+  map_url VARCHAR(255),
+  city_url VARCHAR(255),
+  time VARCHAR(255),
+  hotel_name VARCHAR(255),
+  hotel_price VARCHAR(255),
+  hotel_rate VARCHAR(255),
+  hotel_img VARCHAR(255),
+  station_name VARCHAR(255),
+  station_type VARCHAR(255),
+  transport_price VARCHAR(255)
 );
--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const pg = require('pg');
 const DATABASE_URL = process.env.DATABASE_URL;
 const NODE_ENV = process.env.NODE_ENV;
 const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
-const client = new pg.Client(options);
+const Client = new pg.Client(options);
 
 const methodOverride = require('method-override');
+const { forEach, result } = require('underscore');
 
 // setupes
 const PORT = process.env.PORT || 3030;
@@ -29,44 +30,85 @@ app.get('/', (req, res) => {
 
 app.get('/search', allApiHandler);
 
-app.get('/covid', covidHandler);
+// app.get('/covid', covidHandler);
+
+//DataBase Route
+app.get('/saveData', saveDataHandler);
+app.get('/allSavedData', allSavedDataHandler);
+app.post('/details/:PlanID', showPlanDetails);
+app.put('/updatePlan/:planID',updatePlanHandler);
+app.delete('/deletePlan/:planID',deletePlanHandler);
 
 // function handler
 
-
 function allApiHandler(req, res) {
   let appAPiArr = [];
+  
 
-  console.log('allApiHandler');
-  console.log(req.query);
-  let a = locationHandler(req, res)
-    .then(data1 => {
-      console.log('inside-locationHandler------------', data1);
-      appAPiArr.push(data1);
-      let b = weatherHandler(req, res)
-        .then(data2 => {
-          console.log('inside-weatherHandler------------', data2);
-          appAPiArr.push(data2);
-          let c = findHotelsHandler(req, res)
-            .then(data3 => {
-              console.log('inside-findHotelsHandler------------', data3);
-              appAPiArr.push(data3);
-              let d = findTransportHandler(req, res)
-                .then(data4 => {
-                  console.log('inside-findTransportHandler------------', data4);
-                  appAPiArr.push(data4);
-                  console.log('1111111111111111111111111111111111111111111111111', data2.country);
-                  let f = covidHandler(data2.country)
-                    .then(data5 => {
-                      console.log('inside-covidHandler------------', data5);
-                      appAPiArr.push(data5);
-                      res.render('pages/result', { apiAll: appAPiArr });
-                    });
-                });
-            });
-        });
-    });
-  console.log('**********************');
+  // console.log('allApiHandler');
+  // console.log(req.query);
+  let SQL ='SELECT id FROM city WHERE city =$1;';
+  let safeValue = [req.query.search];
+  Client.query(SQL,safeValue).then(data =>{
+    console.log(!(data.rows.length));
+    if(!(data.rows.length)){
+      let SQL= 'insert into city (city) values($1) returning id;';
+      let safevalue=[req.query.search];
+      Client.query(SQL,safevalue).then(data0=>{
+        console.log(data0.rows[0].id)
+        let a = locationHandler(req, res,data0.rows[0].id)
+          .then(data1 => {
+            // console.log('inside-locationHandler------------', data1);
+            appAPiArr.push(data1);
+            let b = weatherHandler(req, res)
+              .then(data2 => {
+                // console.log('inside-weatherHandler------------', data2);
+                appAPiArr.push(data2);
+                let c = findHotelsHandler(req, res,data0.rows[0].id)
+                  .then(data3 => {
+                  // console.log('inside-findHotelsHandler------------', data3);
+                    appAPiArr.push(data3);
+                    let d = findTransportHandler(req, res,data0.rows[0].id)
+                      .then(data4 => {
+                        // console.log('inside-findTransportHandler------------', data4);
+                        appAPiArr.push(data4);
+                        // console.log('1111111111111111111111111111111111111111111111111', data2.country);
+                        let f = covidHandler(data2.country)
+                          .then(data5 => {
+                            // console.log('inside-covidHandler------------', data5);
+                            appAPiArr.push(data5);
+                            res.render('pages/result', { apiAll: appAPiArr });
+                          });
+                      });
+                  });
+              });
+          });
+      });
+    }
+    else{
+      let id =data.rows[0].id;
+      console.log('ttttttttt',id);
+      let SQL ='select * from city as c join locationdata as l on l.city_id = c.id join hoteldata as h on h.city_id=c.id join transportdata as t on t.city_id = c.id where c.id =$1;';
+      let safevalue= [data.rows[0].id];
+      Client.query(SQL,safevalue).then(result =>{
+        result.rows.forEach(ele=>appAPiArr.push(ele));
+        let b = weatherHandler(req, res)
+          .then(data2 => {
+            // console.log('inside-weatherHandler------------', data2);
+            appAPiArr.push(data2);
+            let f = covidHandler(data2.country)
+              .then(data5 => {
+                // console.log('inside-covidHandler------------', data5);
+                appAPiArr.push(data5);
+                res.render('pages/result', { apiAll: appAPiArr });
+              });
+          });
+      });
+    }
+
+  });
+
+  // console.log('**********************');
   // findHotelsHandler(req, res)
   //   .then(data1 => {
 
@@ -84,12 +126,12 @@ function allApiHandler(req, res) {
 }
 
 
-function locationHandler(req, res) {
-  console.log('----------------------------------------------------------------------------------');
-  console.log('locationHandler');
+function locationHandler(req, res,id) {
+  // console.log('----------------------------------------------------------------------------------');
+  // console.log('locationHandler');
   let keyVal = process.env.PIXABAY_KEY;
 
-  console.log(req.query.text);
+  // console.log(req.query.text);
 
   let text = req.query.search;
 
@@ -113,11 +155,18 @@ function locationHandler(req, res) {
             arr.push(nnata[idx]);
             //console.log('sdsdds', arr);
           }
-          //console.log('arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr')
-          //console.log('arrrr', arr, nnata);
+          let map_url = arr[1];
+          let city_url= arr[2].photoLink;
+          let SQL= 'INSERT INTO locationdata ( map_url , city_url,city_id) VALUES ($1,$2,$3);';
+          let safeValues= [map_url,city_url,id];
+          Client.query(SQL,safeValues).then(data=> console.log('done'));
+
+          // console.log('arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr')
+          // console.log('arrrr', arr, nnata);
           //res.render('pages/test', { allData: arr });
           return arr;
         })
+
         .catch(error => {
           //console.log('Error in getting data from Google Books server')
           console.error(error);
@@ -214,8 +263,8 @@ function getPhoto(keyVal, text) {
 
 
 function weatherHandler(req, res) {
-  console.log('----------------------------------------------------------------------------------');
-  console.log('weatherHandler');
+  // console.log('----------------------------------------------------------------------------------');
+  // console.log('weatherHandler');
   let key = process.env.WEATHER_KEY;
   let cityName = req.query.search;
   cityName.charAt(0).toUpperCase() + cityName.slice(1);
@@ -223,9 +272,9 @@ function weatherHandler(req, res) {
   return superagent(url)
     .then(result => {
       let dataWeather = result.body;
-      console.log(dataWeather);
+      // console.log(dataWeather);
       let myData = new Weather(dataWeather);
-      console.log('fffffffffffffffffffffff');
+      // console.log('fffffffffffffffffffffff');
 
       //res.render('pages/test', { bookDataArray: myData });
       // res.send(myData);
@@ -235,23 +284,35 @@ function weatherHandler(req, res) {
 }
 
 
-function findHotelsHandler(req, res) {
-  console.log('----------------------------------------------------------------------------------');
-  console.log('locationHandler');
+function findHotelsHandler(req, res,id) {
+  // console.log('----------------------------------------------------------------------------------');
+  // console.log('locationHandler');
   let hotelsArray = [];
   let cityName = req.query.search;
   let hotelsKey = process.env.HOTELS_KEY;
   let hotelsURL = `https://hotels-com-provider.p.rapidapi.com/v1/destinations/search?locale=en_US&currency=USD&query=${cityName}&rapidapi-key=${hotelsKey}`;
+  let photoKey = process.env.PIXABAY_KEY;
   return superagent.get(hotelsURL) //send request to weatherbit.io API
     .then(geoData => {
-      console.log('inside superagent');
+      // console.log('inside superagent');
       // console.log(geoData);
       // let gData= geoData.body.searchResults.results;
       let gData = geoData.body.suggestions[1].entities;
 
       gData.map(val => {
-        const newHotel = new Hotel(val);
-        hotelsArray.push(newHotel);
+        let imagLink = getPhoto(photoKey, val.name)
+          .then(data => {
+            const newHotel = new Hotel(val, data);
+            hotelsArray.push(newHotel);
+          });
+
+      });
+
+      hotelsArray.forEach(element => {
+        let SQL= 'INSERT INTO hoteldata ( hotel_image, hotel_name , hotel_price , hotel_rate,city_id) VALUES ($1,$2,$3,$4,$5);';
+
+        let safeValues= [element.hotel_image,element.hotel_name,element.hotel_price,element.hotel_rate,id];
+        Client.query(SQL,safeValues).then(data=> console.log('done'));
       });
       //res.send(hotelsArray);
       return gData;
@@ -259,24 +320,24 @@ function findHotelsHandler(req, res) {
     })
 
     .catch(error => {
-      console.log('inside superagent');
-      console.log('Error in getting data from hotels server');
+      // console.log('inside superagent');
+      // console.log('Error in getting data from hotels server');
       console.error(error);
       //res.send(error);
     });
-  console.log('after superagent');
+  // console.log('after superagent');
 }
 
 
-function findTransportHandler(req, res) {
+function findTransportHandler(req, res,id) {
   let transArray = [];
   let cityName = req.query.search;
   let transKey = process.env.TRANS_KEY;
   let hotelsURL = `https://hotels-com-provider.p.rapidapi.com/v1/destinations/search?locale=en_US&currency=USD&query=${cityName}&rapidapi-key=${transKey}`;
   return superagent.get(hotelsURL) //send request to weatherbit.io API
     .then(geoData => {
-      console.log('----------------------------------------------------------------------------------');
-      console.log('insigeoDatade superagent');
+      // console.log('----------------------------------------------------------------------------------');
+      // console.log('insigeoDatade superagent');
       // console.log(geoData);
       // let gData= geoData.body.searchResults.results;
       let gData = geoData.body.suggestions[3].entities;
@@ -285,15 +346,21 @@ function findTransportHandler(req, res) {
         transArray.push(newTrans);
       });
       //res.send(transArray);
+      transArray.forEach(element => {
+        let SQL= 'INSERT INTO transportdata ( station_name, station_type , transport_price,city_id) VALUES ($1,$2,$3,$4);';
+
+        let safeValues= [element.station_name,element.station_type,element.transport_price,id];
+        Client.query(SQL,safeValues).then(data=> console.log('done'));
+      });
       return gData;
     })
     .catch(error => {
-      console.log('inside superagent');
-      console.log('Error in getting data from hotels server');
+      // console.log('inside superagent');
+      // console.log('Error in getting data from hotels server');
       console.error(error);
       //res.send(error);
     });
-  console.log('after superagent');
+  // console.log('after superagent');
 }
 
 
@@ -314,8 +381,62 @@ function covidHandler(req) {
 
 
 
+// DataBase Functions
+
+function saveDataHandler(req, res) {
+  let { city, map_url, hotel_name, station_name, time } = req.query;
+  let SQL = `INSERT INTO booking (city, map_url, hotel_name, station_name, time) VALUES ($1, $2, $3, $4, $5);`;
+  let safeValues = [city, map_url, hotel_name, station_name, time];
+  Client.query(SQL, safeValues)
+    .then(data => {
+      res.redirect('/allSavedData');
+    });
+}
+
+function allSavedDataHandler(req, res) {
+  let SQL = 'SELECT * FROM booking;';
+  Client.query(SQL)
+    .then(data => {
+      res.render('pages/saved', { bookingData: data });
+    });
+}
+
+function deletePlanHandler (req,res){
+  let SQL = `DELETE FROM booking WHERE id=$1;`;
+  let value = [req.params.planID];
+  Client.query(SQL,value)
+    .then(res.redirect('/'));
+  // res.render(`pages/saved`,{obj:safeValues});
+
+}
+
+function showPlanDetails (req,res){
+  let SQL = `SELECT * FROM booking WHERE id=$1;`;
+  let value = [req.params.PlanID];
+  Client.query(SQL,value)
+    .then((result) => {
+      res.render('pages/details',{plan:result.rows[0]});
+    });
+}
+
+function updatePlanHandler (req,res){
+  // console.log(req.params.PlanID);
+  let {city,image_url,date,hotel,transport} = req.body;
+  let SQL = `UPDATE booking SET city=$1,image_url=$2,date=$3,hotel=$4,transport=$5 WHERE id=$6;`;
+  let safeValues = [city,image_url,date,hotel,transport,req.params.PlanID];
+  Client.query(SQL,safeValues)
+    .then(()=>{
+      res.redirect(`/`);
+      // res.render(`pages/saved`,{obj:safeValues});
+      res.redirect(`/details/${req.params.planID}`);
 
 
+
+    }).catch(error=>{
+      res.send(error);
+    });
+
+}
 
 
 //constructors
@@ -325,7 +446,7 @@ function Covid(covidData) {
   this.confirmed = covidData.confirmed;
   this.recovered = covidData.recovered;
   this.deaths = covidData.deaths;
-  this.updated = covidData.updated;
+  this.updated = (covidData.updated)?covidData.updated:'NO DATA';
 }
 
 const CityPhoto = function (photoLink) {
@@ -375,14 +496,27 @@ function Weather(result) {
   this.weather_descriptions = result.current.weather_descriptions;
 }
 
-function Hotel(hotelData) {
-  this.name = hotelData.name;
+function Hotel(hotelData, data) {
+  // console.log('----------------------------------------------------------------------------------');
+  // console.log('Hotel');
+  let minRate = 1;
+  let maxRate = 5;
+  let minPrice = 20;
+  let maxPrice = 300;
+  this.hotel_name = hotelData.name;
+  this.hotel_image = (data.length) ? data : 'https://image.freepik.com/free-vector/lifestyle-hotel-illustration_335657-398.jpg';
+  this.hotel_rate = Math.floor(Math.random() * (maxRate - minRate) + minRate);
+  this.hotel_price = Math.floor(Math.random() * (maxPrice - minPrice) + minPrice);
+  // console.log('this.name--->', this.name, 'this.imageLinks---->', this.imageLinks, this.rate, this.price);
 }
 
 
 function Transport(transData) {
-  this.name = transData.name;
-  this.type = transData.type;
+  let minPrice = 10;
+  let maxPrice = 50;
+  this.station_name = transData.name;
+  this.station_type = transData.type;
+  this.transport_price = Math.floor(Math.random() * (maxPrice - minPrice) + minPrice);
 }
 
 // let ctx = document.getElementById( 'myChart' ).getContext( '2d' );
@@ -428,9 +562,9 @@ app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
 
 //listen
-// client.connect()
-//   .then(() => {
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
-// });
+Client.connect()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+  });
 
 // add dev branch
